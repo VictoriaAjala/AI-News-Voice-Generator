9af4bcd31f24f0cd73f4511b9d146b1871c9dff5
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-11-18 00:00:26 +0100

    alan func

--- voice/src/App.js
+++ voice/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = '';
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+        })
+    }, [])
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
         </div>
     )
 }
+
+export default App;
